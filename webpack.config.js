9511eac69f2d89b51cb30f5cfe78fa237f1480e2
moisesmartinezliranzo <moisesmartinezliranzo@gmail.com>
2019-05-17 13:35:37 -0400

    Flex box

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'src'),
+        watchContentBase: true,
+        port: 9000
+    }
+}
